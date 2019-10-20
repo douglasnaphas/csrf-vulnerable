a53311a9def90508b56dfbc7091ce363a2109dab
SierraS <schellenbergsierra@gmail.com>
2019-10-20 16:22:25 -0400

    add source and public folders, buildspec update

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./App.css";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+function App() {
+  const [url, setUrl] = React.useState("initial url");
+  const [counter, setCounter] = React.useState(0);
+  return (
+    <div className="App">
+      <Typography variant="h2" component="h1">
+        CSRFX
+      </Typography>
+      <Typography variant="h3" component="h3">
+        Attempt Cross-Site Request Forgeries to test defenses
+      </Typography>
+      <br />
+      <br />
+      <Paper className="White-paper" style={{ minWidth: "400px" }}>
+        <Typography variant="h5" component="h3">
+          Enter a URL
+        </Typography>
+        <div>
+          <br />
+          <TextField
+            label="URL:"
+            fullWidth
+            onChange={event => {
+              setUrl(event.target.value);
+            }}
+          ></TextField>
+        </div>
+        <br />
+        <br />
+        <div>
+          <Button
+            variant="contained"
+            onClick={() => {
+              console.log(url);
+              fetch(url).then(r => {
+                console.log(r.status);
+              });
+            }}
+          >
+            Execute
+          </Button>
+        </div>
+      </Paper>
+    </div>
+  );
+}
+
+export default App;
