76b8fb95115d1fbdb2b357966035f66a863def20
SierraS <schellenbergsierra@gmail.com>
2019-10-20 20:09:01 -0400

    Set up initial view for victim site

--- src/App.js
+++ src/App.js
@@ -7,44 +7,20 @@ import Button from "@material-ui/core/Button";
 
 function App() {
   const [url, setUrl] = React.useState("initial url");
-  const [counter, setCounter] = React.useState(0);
   return (
     <div className="App">
       <Typography variant="h2" component="h1">
-        CSRFX
+        Vulnemort
       </Typography>
       <Typography variant="h3" component="h3">
-        Attempt Cross-Site Request Forgeries to test defenses
+        A site vulnerable to Cross-Site Request Forgery
       </Typography>
       <br />
       <br />
       <Paper className="White-paper" style={{ minWidth: "400px" }}>
-        <Typography variant="h5" component="h3">
-          Enter a URL
-        </Typography>
         <div>
-          <br />
-          <TextField
-            label="URL:"
-            fullWidth
-            onChange={event => {
-              setUrl(event.target.value);
-            }}
-          ></TextField>
-        </div>
-        <br />
-        <br />
-        <div>
-          <Button
-            variant="contained"
-            onClick={() => {
-              console.log(url);
-              fetch(url).then(r => {
-                console.log(r.status);
-              });
-            }}
-          >
-            Execute
+          <Button variant="contained" onClick={() => {}}>
+            Get a cookie
           </Button>
         </div>
       </Paper>
