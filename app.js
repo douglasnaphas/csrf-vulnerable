31a51b9e8fc22005039b57dda106222cf9559ea3
SierraS <schellenbergsierra@gmail.com>
2019-10-21 20:58:48 -0400

    Fix spacing on login, CSRF midware

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 const getLoginCookie = require("./middleware/getLoginCookie");
 const getCSRFCookie = require("./middleware/getCSRFCookie");
 
-app.get('/', function(req, res) {
+app.get("/", function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    Output: "Hello World!"
   });
 });
 
-app.post('/', function(req, res) {
+app.post("/", function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    Output: "Hello World!"
   });
 });
 
@@ -20,7 +20,6 @@ app.get("/login-cookie", getLoginCookie);
 app.get("/csrf-cookie", getCSRFCookie);
 
 app.get("/cookie", getLoginCookie);
-
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app;
--- middleware/getCSRFCookie.js
+++ middleware/getCSRFCookie.js
@@ -1,20 +1,21 @@
 const funcArray = [
   function(req, res, next) {
-    res.cookie("CSRF-Token", (() => {
-      let cook = '';
-      const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      for(let i = 0; i < 6; i++) {
-        cook += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-      }
-      return cook;
-    })());
+    res.cookie(
+      "CSRF-Token",
+      (() => {
+        let cook = "";
+        const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (let i = 0; i < 6; i++) {
+          cook += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        }
+        return cook;
+      })()
+    );
     next();
-   },
-    function(req, res) {
-        res.send({"Output": "You have received a cookie"});
-    }
+  },
+  function(req, res) {
+    res.send({ Output: "You have received a cookie" });
+  }
 ];
-
-
 
 module.exports = funcArray;
