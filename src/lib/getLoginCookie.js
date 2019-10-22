9a0c011a53872520bd9036e8677a5aea6d7edaf4
Douglas Naphas <douglasnaphas@gmail.com>
2019-10-22 16:39:48 -0400

    Request credentials in fetch to login-cookie
    This is necessary for fetch to receive cookies.

--- src/lib/getLoginCookie.js
+++ src/lib/getLoginCookie.js
@@ -2,7 +2,7 @@ import { Configs } from "../Configs";
 
 async function getLoginCookie() {
   const getCookie = new URL(`/login-cookie`, Configs.apiUrl());
-  const response = await fetch(getCookie);
+  const response = await fetch(getCookie, { credentials: "include" });
   const data = await response.json();
   const status = response.status;
   return {
